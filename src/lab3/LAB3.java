b662026099753e2647a4c972c5d56759fa34d80b
neveenbarada <g00058010@aus.edu>
2017-10-01 11:46:39 +0400

    Initial Version

---
+++ src/lab3/LAB3.java
@@ -0,0 +1,14 @@
+
+package lab3;
+
+/**
+ *
+ * @author g00058010
+ */
+public class LAB3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello there.");
+    }
+    
+}
